4ecd6138db729d203723133a9a72767d1295923d
Niraj Pahari <theniraj23@gmail.com>
2020-08-24 11:04:56 +0700

    build and devel removed

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/niraj/MyComputer/StudyStuff/RoboticsLab3/workspaces/catkin_ws/src
-# Build directory: /home/niraj/MyComputer/StudyStuff/RoboticsLab3/workspaces/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
